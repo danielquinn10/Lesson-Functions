8beb81e7c1adebea20e6677c2e9d18d2955428f1
MrAPauls <55466660+MrAPauls@users.noreply.github.com>
2021-02-05 14:07:56 -0400

    Create Functions_Lesson.py

---
+++ Functions_Lesson.py
@@ -0,0 +1,20 @@
+##Computers and Python
+##Functions
+
+#Four reasons to use functions
+#1.
+#2.
+#3.
+#4.
+
+#For the following function..
+#argument:
+#parameter:
+#results:
+def add_two(a, b):
+    x = a + b
+    return x
+
+add_two(3, 4)
+
+
